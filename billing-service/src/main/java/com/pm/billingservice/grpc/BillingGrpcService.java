cb7aa6f3cfae2cf6c1cd05f6cbba8c2c3cdfd5e2
hamdiboyraz <hamdiboyraz19@gmail.com>
2025-03-15 13:40:18 +0300

    Add billing-service proto file and service

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+        log.info("createBillingAccount request received: {}", request.toString());
+
+        // Business logic to create billing account - e.g. save to database, perform calculates etc.
+
+        // Dummy response
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("123")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
